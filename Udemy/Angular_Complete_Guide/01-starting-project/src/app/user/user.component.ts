e0728a95236e2c59d8482e312469cb8393512446
diegokoes <stopwatch1567@proton.me>
2025-11-22 18:12:03 +0100

    Refactor code structure for improved readability and maintainability

---
+++ Udemy/Angular_Complete_Guide/01-starting-project/src/app/user/user.component.ts
@@ -0,0 +1,34 @@
+import { Component, computed, input } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
+  // onSelectUser() {
+  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  //   //this.selectedUser = DUMMY_USERS[randomIndex]; without signal
+  // }
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
+  // get imagePath() {
+  //   return 'assets/users/' + this.avatar;
+  // }
+  imagePath = computed(() => 'assets/users/' + this.avatar);
+  onSelectUser() {
+    //this.avatar.set(); error, they are read only, only changed from outisde
+  }
+}
